90090ad9ce0ac42bb797c79675e8bfdc8202415d
Ong Ding Sheng <dong@Ongs-MacBook-Pro.local>
2018-05-08 04:01:50 +0800

    thread

--- emo/views.py
+++ emo/views.py
@@ -8,6 +8,7 @@ from emo.models import Session_data
 import Algorithmia
 import json	
 import time
+import threading
 
 
 # @require_http_methods(["GET", "POST"])
@@ -16,12 +17,13 @@ def init_sess(request):
 
 	if request.body:
 
-		usr_data = json.dumps({})
 		
 		js = json.loads(request.body)
 		sess_id = js['id']
 		spp = js['ss']
 		total_img = js['tt']
+		usr_data = {str(k): [] for k in range(total_img)}
+		usr_data = json.dumps(usr_data)
 
 		q = Session_data(
 				session_id = sess_id,
@@ -35,6 +37,47 @@ def init_sess(request):
 	return HttpResponse('200')
 
 
+def api_call(sess_id, data, label):
+	try:
+		client = Algorithmia.client("simJoaETq5SHL8t3YIU19pWMfLr1")
+		algo = client.algo('deeplearning/EmotionRecognitionCNNMBP/1.0.1')
+		result = algo.pipe(data).result
+		result = result["results"][0]["emotions"]
+		# result = [{'confidence': 0.1, 'label': 'Neutral'},
+		# 		  {'confidence': 0.7, 'label': 'Disgust'},
+		# 		  {'confidence': 0, 'label': 'Surprise'},
+		# 		  {'confidence': 0, 'label': 'Sad'},
+		# 		  {'confidence': 0, 'label': 'Fear'},
+		# 		  {'confidence': 0, 'label': 'Happy'},
+		# 		  {'confidence': 0, 'label': 'Angry'}]
+		print('Food {}'.format(label))
+		print('\n'.join(['{}'.format(str(k)) for k in result]))
+
+	except:
+		result = [{'confidence': 0, 'label': 'Neutral'},
+				  {'confidence': 0, 'label': 'Disgust'},
+				  {'confidence': 0, 'label': 'Surprise'},
+				  {'confidence': 0, 'label': 'Sad'},
+				  {'confidence': 0, 'label': 'Fear'},
+				  {'confidence': 0, 'label': 'Happy'},
+				  {'confidence': 0, 'label': 'Angry'}]
+	
+	score = cal_score(result)
+
+	
+	q = Session_data.objects.get(pk=sess_id)
+	while q.lock:
+		q = Session_data.objects.get(pk=sess_id)
+		time.sleep(1 / 10)
+	q.lock = 1
+	q.save()
+	usr_data = json.loads(q.usr_data)
+	usr_data[label].append(score)
+	q.usr_data = json.dumps(usr_data)
+	q.lock = 0
+	q.save()
+
+
 # @require_http_methods(["GET", "POST"])
 @csrf_exempt
 def upload_img(request):
@@ -45,65 +88,56 @@ def upload_img(request):
 		
 		sess_id = js['ses']
 
-		q = Session_data.objects.get(pk=sess_id)
-		while q.lock:
-			time.sleep(1 / 10)
-		q.lock = 1
-		q.save()
-
-		usr_data = json.loads(q.usr_data)
-		spp = q.spp
+		spp = js['spp']
 		img = js['img']
 		label = str(js['lbl'] // spp)
-		if not label in usr_data.keys():
-			usr_data[label] = []
-		q.usr_data = json.dumps(usr_data)
-		q.lock = 0
-		q.save()
-		
-
 
 		data = { 
 			  "image": img
 			, "numResults": 7
 		}
-		try:
-			client = Algorithmia.client("simJoaETq5SHL8t3YIU19pWMfLr1")
-			algo = client.algo('deeplearning/EmotionRecognitionCNNMBP/1.0.1')
-			result = algo.pipe(data).result
-			result = result["results"][0]["emotions"]
-			# result = [{'confidence': 0.1, 'label': 'Neutral'},
-			# 		  {'confidence': 0.7, 'label': 'Disgust'},
-			# 		  {'confidence': 0, 'label': 'Surprise'},
-			# 		  {'confidence': 0, 'label': 'Sad'},
-			# 		  {'confidence': 0, 'label': 'Fear'},
-			# 		  {'confidence': 0, 'label': 'Happy'},
-			# 		  {'confidence': 0, 'label': 'Angry'}]
-			print('Food {}'.format(label))
-			print('\n'.join(['{}'.format(str(k)) for k in result]))
+		# try:
+		# 	client = Algorithmia.client("simJoaETq5SHL8t3YIU19pWMfLr1")
+		# 	algo = client.algo('deeplearning/EmotionRecognitionCNNMBP/1.0.1')
+		# 	result = algo.pipe(data).result
+		# 	result = result["results"][0]["emotions"]
+		# 	# result = [{'confidence': 0.1, 'label': 'Neutral'},
+		# 	# 		  {'confidence': 0.7, 'label': 'Disgust'},
+		# 	# 		  {'confidence': 0, 'label': 'Surprise'},
+		# 	# 		  {'confidence': 0, 'label': 'Sad'},
+		# 	# 		  {'confidence': 0, 'label': 'Fear'},
+		# 	# 		  {'confidence': 0, 'label': 'Happy'},
+		# 	# 		  {'confidence': 0, 'label': 'Angry'}]
+		# 	print('Food {}'.format(label))
+		# 	print('\n'.join(['{}'.format(str(k)) for k in result]))
 
-		except:
-			result = [{'confidence': 0, 'label': 'Neutral'},
-					  {'confidence': 0, 'label': 'Disgust'},
-					  {'confidence': 0, 'label': 'Surprise'},
-					  {'confidence': 0, 'label': 'Sad'},
-					  {'confidence': 0, 'label': 'Fear'},
-					  {'confidence': 0, 'label': 'Happy'},
-					  {'confidence': 0, 'label': 'Angry'}]
+		# except:
+		# 	result = [{'confidence': 0, 'label': 'Neutral'},
+		# 			  {'confidence': 0, 'label': 'Disgust'},
+		# 			  {'confidence': 0, 'label': 'Surprise'},
+		# 			  {'confidence': 0, 'label': 'Sad'},
+		# 			  {'confidence': 0, 'label': 'Fear'},
+		# 			  {'confidence': 0, 'label': 'Happy'},
+		# 			  {'confidence': 0, 'label': 'Angry'}]
 		
-		score = cal_score(result)
+		# score = cal_score(result)
 
 		
-		q = Session_data.objects.get(pk=sess_id)
-		while q.lock:
-			time.sleep(1 / 10)
-		q.lock = 1
-		q.save()
-		usr_data = json.loads(q.usr_data)
-		usr_data[label].append(score)
-		q.usr_data = json.dumps(usr_data)
-		q.lock = 0
-		q.save()
+		# q = Session_data.objects.get(pk=sess_id)
+		# while q.lock:
+		# 	q = Session_data.objects.get(pk=sess_id)
+		# 	time.sleep(1 / 10)
+		# q.lock = 1
+		# q.save()
+		# usr_data = json.loads(q.usr_data)
+		# usr_data[label].append(score)
+		# q.usr_data = json.dumps(usr_data)
+		# q.lock = 0
+		# q.save()
+		t = threading.Thread(target=api_call, args=(sess_id, data, label, ), daemon=True)
+		t.start()
+		t.join()
+
 
 	return HttpResponse('200')
 
@@ -122,16 +156,17 @@ def get_res(request):
 		spp = q.spp
 			
 		sum_of_pics = sum([len(usr_data[k]) for k in usr_data])
-		start = time.time()
-		dur = 0
-		while sum_of_pics < total_img * spp and dur < 25:
-			q = Session_data.objects.get(pk=sess_id)
+		# start = time.time()
+		# dur = 0
+		# while sum_of_pics < total_img * spp and dur < 25:
+		# 	q = Session_data.objects.get(pk=sess_id)
 			
-			usr_data = json.loads(q.usr_data)
-			sum_of_pics = sum([len(usr_data[k]) for k in usr_data])
-			dur = time.time() - start
-			time.sleep(1 / 10)
-		if dur >= 25:
+		# 	usr_data = json.loads(q.usr_data)
+		# 	sum_of_pics = sum([len(usr_data[k]) for k in usr_data])
+		# 	dur = time.time() - start
+		# 	time.sleep(1 / 10)
+		if sum_of_pics < total_img * spp:
+			print(sum_of_pics, usr_data)
 			return HttpResponse(json.dumps({'result': []}))
 			
 	
