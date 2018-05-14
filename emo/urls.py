27622d6617eb63aa63d3a5f0e282bf7f6b9ccfdf
Desmond Yeoh Teik Boon <dyeoh@MacBook-Pro.local>
2018-05-14 19:56:32 +0800

    SSL

--- emo/urls.py
+++ emo/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('acme-challenge/PVvfKwSeozsLTafSnIYC2U6RSsWGBcikZUBgZBfoUIQ', views.ssl, name='ssl'),
+	path('acme-challenge/S8Y8ZXB9tqoeUPLQhx83QHLGswlAx_cKEzrnM3sodVI', views.ssl, name='ssl'),
 	path('index', views.index, name="index"),
 	path('upload', views.upload_img, name="upload"),
 	path('init', views.init_sess, name="init"),
